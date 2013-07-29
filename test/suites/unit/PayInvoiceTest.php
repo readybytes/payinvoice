661a6e1ea18dd05c344969ba64388ae6f23ae132
Gaurav Jain <gaurav@readybytes.in>
2013-07-29 19:29:07 +0530

    testing framework added

---
+++ test/suites/unit/PayInvoiceTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package     Joomla.UnitTest
+ * @subpackage  User
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+// load the file which is required
+
+/**
+ * Tests for the JAuthentication class.
+ *
+ * @package     Joomla.UnitTest
+ * @subpackage  Utilities
+ * @since       11.1
+ *
+ * @runInSeparateProcess
+ */
+class PayInvoiceTest extends TestCase
+{	
+	public function testAuthorise()
+	{
+		$this->assertTrue(true);
+		return true;
+	}
+}
