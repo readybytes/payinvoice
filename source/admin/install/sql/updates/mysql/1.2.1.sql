17cfe937ef38ab149b74ca2e7e797c35040be10b
dineshreadybytes <diensh@readybytes.in>
2015-11-09 11:52:57 +0530

    Improvement::Improve install update sql

--- source/admin/install/sql/updates/mysql/1.2.1.sql
+++ source/admin/install/sql/updates/mysql/1.2.1.sql
@@ -4,7 +4,6 @@
 -- 
 -- --------------------------------------------------------
 
-
 CREATE TABLE IF NOT EXISTS `#__payinvoice_item` (
 `item_id` int(11) NOT NULL AUTO_INCREMENT,
 `type` varchar(255) NOT NULL,
@@ -21,11 +20,11 @@ INSERT INTO `#__payinvoice_item`(`type`, `title`) VALUES ('task' , 'General'), (
 CREATE TABLE IF NOT EXISTS `#__payinvoice_invoice_x_item` (
  `invoice_id` int(11) NOT NULL, 
 `item_id` int(11) NOT NULL,
-`type` varchar(255) NOT NULL,
 `title` varchar(255) NOT NULL,
+`type` varchar(255) NOT NULL,
 `unit_cost` decimal(15,5) NOT NULL,
 `quantity` int(11) NOT NULL,
 `tax` decimal(15,5) NOT NULL,
 `line_total` decimal(15,5) NOT NULL,
-`created_date` datetime,
+`created_date` datetime
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
